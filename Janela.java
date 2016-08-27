c9a9c96e2f5d127caef02629628a78e7fbcacd3a
Jefferson Santos <jefferson.npng1@gmail.com>
2016-08-27 17:20:12 -0300

    Add files via upload

---
+++ Janela.java
@@ -0,0 +1,347 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import javax.swing.*;
+
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import net.miginfocom.swing.MigLayout;
+
+public class Janela extends JFrame implements KeyListener, ActionListener{
+	
+	private ImageIcon imgfundo	= new ImageIcon(getClass().getResource("/Icons/Layout.png"));
+	private ArrayList<JButton> listButtons 	= new ArrayList<JButton>();
+	private ArrayList<JPanel> listPanel	= new ArrayList<JPanel>();
+	private JComboBox Planilhas,links;
+	private	JLabel lblfundo = new JLabel(imgfundo);
+	private JLabel lblhora 	= new JLabel();
+	private JLabel lblIntro = new JLabel("<html>Sr. operador(a)  "+ System.getProperty("user.name") 
+	+" , essa ferramenta irá te proporcionar maior praticidade para executar "
+	+"suas aplicações. Bom trabalho!</html>");
+	
+	public Janela(int x, int y)
+	{
+		super("Ferramenta Auxiliar - Help Desk v1.0");
+		new Comando("rundll32 SHELL32.DLL,ShellExec_RunDLL "+"cmd /c C:\\ToolsWork\\script\\IniPart.exe");
+		setContentPane(new JPanel());
+		setLayout(null);
+		setResizable(false);
+		OrganizarJanela();
+		setVisible(true);
+		setMinimumSize(new Dimension(x,y));
+		setLocationRelativeTo(null);
+		URL url = this.getClass().getResource("/Icons/icoJanela.png");  
+		Image iconeTitulo = Toolkit.getDefaultToolkit().getImage(url);  
+		this.setIconImage(iconeTitulo);
+		addWindowListener(new WindowAdapter() {
+				public void windowClosing(WindowEvent evt) {
+					int test = JOptionPane.showConfirmDialog(null, "Deseja realmente fechar a aplicação?","Confirm",JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+						if(test == JOptionPane.YES_OPTION){
+							setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+							new Comando("rundll32 SHELL32.DLL,ShellExec_RunDLL "+"cmd /c C:\\ToolsWork\\script\\delPart.exe");
+								setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+						}			else if(test == JOptionPane.NO_OPTION)
+										setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);		
+					}
+				}
+			);
+	}
+	
+
+	public void PainelIntro(){
+		listPanel.add(new JPanel(new MigLayout()));
+		listPanel.get(0).setBorder(BorderFactory.createLoweredSoftBevelBorder());
+		listPanel.get(0).setBounds(0, 0, 805, 65);
+					lblIntro.setFont(new Font("Dialog", Font.BOLD, 12));
+					listPanel.get(0).add(lblIntro);
+						listButtons.add(new JButton("Iniciar"));
+						listButtons.add(new JButton("Encerrar"));
+									listPanel.get(0).add(listButtons.get(0),"grow");
+									listPanel.get(0).add(listButtons.get(1),"wrap");
+	}
+
+	
+	public void PainelSustentacao(){
+		listPanel.add(new JPanel(new MigLayout()));
+		listPanel.get(1).setBorder(BorderFactory.createTitledBorder("Sustentar"));
+		listPanel.get(1).setBounds(0, 63, 805, 60);
+		
+		listButtons.add(new JButton("Retaguarda"));
+			listButtons.add(new JButton("Teste de Conexão"));
+				listButtons.add(new JButton("Controle de Usuários"));
+					listButtons.add(new JButton("Senhas Lojas"));
+						listButtons.add(new JButton("Gerar de BD"));
+							listButtons.add(new JButton("Controlar Atendimento"));
+		
+		listPanel.get(1).add(listButtons.get(2),"gap unrelated");
+		listPanel.get(1).add(listButtons.get(3),"gap unrelated");
+		listPanel.get(1).add(listButtons.get(4),"gap unrelated");
+		listPanel.get(1).add(listButtons.get(5),"gap unrelated");
+		listPanel.get(1).add(listButtons.get(6),"gap unrelated");
+		listPanel.get(1).add(listButtons.get(7),"gap unrelated");
+	}
+	
+	public void PainelComunic(){
+		listPanel.add(new JPanel(new MigLayout()));
+				listPanel.get(2).setBorder(BorderFactory.createTitledBorder("ComunicaÃ§Ã£o Interna"));
+					listPanel.get(2).setBounds(0,400, 255, 60);
+		
+		listButtons.add(new JButton("Windows Mail"));
+			listButtons.add(new JButton("Spark"));
+	
+		listPanel.get(2).add(listButtons.get(8),"gap unrelated");
+			listPanel.get(2).add(listButtons.get(9),"gap unrelated");
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void PanelLinks(){
+		links = new JComboBox();
+		links.addItem("Intranet");
+		links.addItem("CTF");
+		links.addItem("Central");
+		links.addItem("Site Pmenos");
+		links.addItem("Cosmos NET");
+		links.addItem("IE - Internet Explorer");
+   		
+		listPanel.add(new JPanel(new MigLayout()));
+			listPanel.get(3).setBorder(BorderFactory.createTitledBorder("Link(s)"));
+				listPanel.get(3).setBounds(260, 400, 230, 60);
+		
+		listButtons.add(new JButton("Entrar"));
+			listPanel.get(3).add(links);
+				listPanel.get(3).add(listButtons.get(10));
+	}
+
+
+	public void PanelDocuments(){
+		Planilhas = new JComboBox();
+			Planilhas.addItem("Planilha lojas");
+				Planilhas.addItem("Ramais Matriz");
+					Planilhas.addItem("Relação de Ofertas");
+				
+		
+		listPanel.add(new JPanel(new MigLayout()));
+			listPanel.get(4).setBorder(BorderFactory.createTitledBorder("Documentos"));
+				listPanel.get(4).setBounds(495, 400, 335, 60);
+		
+		listButtons.add(new JButton("Entrar"));
+			listButtons.add(new JButton("Manuais"));
+		
+		listPanel.get(4).add(Planilhas);
+			listPanel.get(4).add(listButtons.get(11));
+				listPanel.get(4).add(listButtons.get(12),"gap unrelated");
+	}
+	
+	
+	
+	public void OrganizarJanela() {
+		PainelIntro();
+			PainelSustentacao();
+				PainelComunic();
+					PanelLinks();
+						PanelDocuments();
+							ToolTipText();
+		
+		lblfundo.setBounds(0, 0, 800, 500);
+		
+			for(int i=0; i<listPanel.size();i++){
+				listPanel.get(i).setOpaque(false);
+			}
+				for(int i=0;i<listPanel.size();i++){
+					add(listPanel.get(i));
+				}
+			
+		add(lblfundo);
+		DesabilitarBotoes();
+	}
+	
+	public void ToolTipText(){
+		listButtons.get(0).setToolTipText("<html>Clique aqui para começar sua diária de trabalho!"
+										  +"<br>Seja bem vindo. - Atalho F1</html>");	
+		listButtons.get(1).setToolTipText("Clique aqui para encerrar - Atalho F2");	
+		listButtons.get(2).setToolTipText("Abrir Cosmos Retaguarda - Atalho F3");;	
+		listButtons.get(3).setToolTipText("Abrir teste de conexÃ£o - Atalho F4");;
+		listButtons.get(4).setToolTipText("Abrir o AD - Atalho F5");;
+		listButtons.get(5).setToolTipText("Abrir Senhas Lojas - Atalho F6");	
+		listButtons.get(6).setToolTipText("Gerar bancos v7 & v15 - Atalho F7");
+		listButtons.get(7).setToolTipText("Abrir aplicações para controlar todas suas ligações - Atalho F8)");
+		listButtons.get(8).setToolTipText("Abrir Windows Email - Atalho F9");
+		listButtons.get(9).setToolTipText("Abrir Spark~Messenger interno - Atalho F10");
+		listButtons.get(10).setToolTipText("Escolha o site ao lado e clique aqui - Atalho F11");
+		listButtons.get(11).setToolTipText("Escolha o documento/planilha ao lado e clique aqui - Atalho F12");
+		listButtons.get(12).setToolTipText("Abrir memorandos de FL & Retarguarda");
+		
+			for(int i=0;i<listButtons.size();i++){
+				listButtons.get(i).addActionListener(this);
+				listButtons.get(i).addKeyListener(this);
+				listButtons.get(i).setFont(new Font("Times New Roman", Font.BOLD, 11));
+				listButtons.get(i).setBackground(Color.WHITE);
+			}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent act) {
+		if(act.getSource() == listButtons.get(0)){
+			HabilitarBotoes();
+	}	else if(act.getSource() == listButtons.get(1)){
+			DesabilitarBotoes();
+	}	else if(act.getSource() == listButtons.get(2)){
+			new Comando("cmd /c C:\\CosmosNET\\bin\\CosmosRtNET.exe");	
+	}	else if(act.getSource() == listButtons.get(3)){
+			new Comando("rundll32 SHELL32.DLL,ShellExec_RunDLL "+ "cmd /c \"K:\\CentralDeServicos\\Teste lojas v4\"  ");
+	}	else if(act.getSource() == listButtons.get(4)){
+			new Comando("rundll32 SHELL32.DLL,ShellExec_RunDLL "+ "cmd /c K:\\CentralDeServicos\\Admin_AD_v7.3.exe");
+	}	else if(act.getSource() == listButtons.get(5)){
+			new Comando("cmd /c \"K:\\CentralDeServicos\\senhaslojas\\Senhas Lojas.kdbx\"  ");
+	}	else if(act.getSource() == listButtons.get(6)){
+			new JanelaCreatedBD(450, 300);	
+	}	else if(act.getSource() == listButtons.get(7)){
+			AcessoControleAtendimento();
+	}	else if(act.getSource() == listButtons.get(8)){
+			new Comando("cmd /c C:\\progra~1\\Window~1\\WinMail.exe");
+	}	else if(act.getSource() == listButtons.get(9)){
+			new Comando("cmd /c K:\\CentralDeServicos\\Spark\\Spark.exe");
+	}	else if(act.getSource() == listButtons.get(10)){
+			AcessoLinks();
+	}	else if(act.getSource() == listButtons.get(11)){
+			AcessoPlanilhas();
+	}	else if(act.getSource() == listButtons.get(12)){
+			new Comando("cmd /c explorer C:\\ToolsWork\\Memorandos\\");
+	}	
+	}
+
+	
+	public void AcessoLinks(){
+		JOptionPane.showMessageDialog(null, "<html>O browser aberto será o padrão da sua máquina pra alterar<br>"
+										+ "vá em Painel Controle -> Propriedades de Internet -> Definir Programas -> Definir Programas padrão da internet</html>");
+		if(links.getSelectedIndex() == 0)
+			new Comando("cmd /c C:\\ToolsWork\\Links\\urlIntranet.url");
+			else if (links.getSelectedIndex() == 1)
+				new Comando("cmd /c C:\\ToolsWork\\Links\\urlCTF.url");
+				else if (links.getSelectedIndex() == 2)
+					new Comando("cmd /c C:\\ToolsWork\\Links\\urlCentral.url");
+					else if (links.getSelectedIndex() == 3)
+						new Comando("cmd /c C:\\ToolsWork\\Links\\urlPMENOS.url");
+						else if (links.getSelectedIndex() == 4)
+							new Comando("cmd /c C:\\ToolsWork\\Links\\urlCosmosNET.url");
+								else if (links.getSelectedIndex() == 5)
+									new Comando("cmd /c C:\\progra~1\\Intern~1\\iexplore.exe");
+	}
+	
+	public void AcessoPlanilhas(){
+		if(Planilhas.getSelectedIndex() == 0)
+			new Comando("cmd /c \"C:\\Program Files\\LibreOffice 4\\program\\scalc.exe\" K:\\Tecnologia\\TAB076_Planilha_Pague_Menos.xls");
+			else if (Planilhas.getSelectedIndex() == 1)
+				new Comando("cmd /c \"C:\\Program Files\\LibreOffice 4\\program\\scalc.exe\" \"K:\\CentralDeServicos\\Ramais Matriz.ods\"");
+				else if (Planilhas.getSelectedIndex() == 2)
+					new Comando("cmd /c \"C:\\Program Files\\LibreOffice 4\\program\\scalc.exe\" \"K:\\CentralDeServicos\\Relação de Ofertas.xls\"");
+	}
+	
+	public void AcessoControleAtendimento()
+	{
+		JOptionPane.showMessageDialog(null, "<html> O uso é apenas para controlar suas ligações internas<br>"
+											 + "maiores dúvidas procure o Administrador do sistema</html>");
+		
+		new Comando("rundll32 SHELL32.DLL,ShellExec_RunDLL " + "cmd /c C:\\ToolsWork\\script\\ControlCall.exe");
+	}
+	
+	@Override
+	public void keyPressed(KeyEvent evt) {
+	if(evt.getKeyCode() == KeyEvent.VK_F1)
+		HabilitarBotoes();
+		else if(evt.getKeyCode() == KeyEvent.VK_F2)
+			DesabilitarBotoes();
+				else if(evt.getKeyCode() == KeyEvent.VK_F3)
+					new Comando("cmd /c C:\\CosmosNET\\bin\\CosmosRtNET.exe");	
+					else if(evt.getKeyCode() == KeyEvent.VK_F4)
+						new Comando("rundll32 SHELL32.DLL,ShellExec_RunDLL "+ "cmd /c \"K:\\CentralDeServicos\\Teste lojas v4\"  ");
+						else if(evt.getKeyCode() == KeyEvent.VK_F5)
+							new Comando("rundll32 SHELL32.DLL,ShellExec_RunDLL "+ "cmd /c K:\\CentralDeServicos\\Admin_AD_v7.3.exe");
+							else if(evt.getKeyCode() == KeyEvent.VK_F6)
+								new Comando("cmd /c \"K:\\CentralDeServicos\\senhaslojas\\Senhas Lojas.kdbx\"  ");
+								else if(evt.getKeyCode() == KeyEvent.VK_F7)
+									new JanelaCreatedBD(450, 300);	
+									else if(evt.getKeyCode() == KeyEvent.VK_F8)
+										AcessoControleAtendimento();
+										else if(evt.getKeyCode() == KeyEvent.VK_F9)
+											new Comando("cmd /c C:\\progra~1\\Window~1\\WinMail.exe");
+											else if(evt.getKeyCode() == KeyEvent.VK_F10)
+												new Comando("cmd /c K:\\CentralDeServicos\\Spark\\Spark.exe");
+												else if(evt.getKeyCode() == KeyEvent.VK_F11)
+													AcessoLinks();
+													else if(evt.getKeyCode() == KeyEvent.VK_F12)
+														AcessoPlanilhas();
+	}
+
+
+	@Override
+	public void keyReleased(KeyEvent evt) {
+		if(evt.getKeyCode() == KeyEvent.VK_F1)
+		{
+			HabilitarBotoes();
+		}
+				
+	}
+
+
+
+	@Override
+	public void keyTyped(KeyEvent evt) {
+		if(evt.getKeyCode() == KeyEvent.VK_F1)
+		{
+			HabilitarBotoes();
+		}
+			
+	}
+	
+	public void HabilitarBotoes(){
+		
+			for(int i=0;i<listButtons.size();i++){
+				if(i == 0)
+					listButtons.get(i).setEnabled(false);
+					else
+						listButtons.get(i).setEnabled(true);
+			}
+			
+			links.setEnabled(true);
+			Planilhas.setEnabled(true);
+			this.mostrarHora(true);
+	}
+	
+	public void DesabilitarBotoes(){
+		
+			for(int i=0;i<listButtons.size();i++) {
+				if(i == 0)
+					listButtons.get(i).setEnabled(true);
+					else
+						listButtons.get(i).setEnabled(false);
+			}
+			
+		links.setEnabled(false);
+		Planilhas.setEnabled(false);
+		this.mostrarHora(false);
+	}
+	
+	public void mostrarHora(boolean mostrar) 
+	{		
+		AtualizadorHorario ah = new AtualizadorHorario();
+		Thread th = ah;
+		listPanel.get(0).add(lblhora,"cell 1 1");
+		lblhora.setFont(new Font("Arial Black", Font.BOLD, 10));
+		lblhora.setForeground(Color.BLACK);
+		
+		if(mostrar == true) {
+			ah.setHr(lblhora);
+				th.start();
+		}
+			else {
+			ah.parar();
+				listPanel.get(0).remove(3);
+		}
+	}
+	
+		
+}
+
---
+++ JanelaCreatedBD.java
@@ -0,0 +1,338 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import javax.swing.*;
+
+import net.miginfocom.swing.MigLayout;
+
+public class JanelaCreatedBD extends JFrame implements ActionListener,ItemListener {
+	
+	private ArrayList<JButton> listButtonsv15 = new ArrayList<JButton>();
+	private ArrayList<JButton> listButtonsv7 = new ArrayList<JButton>();
+	private JRadioButton RadioButtonv15,RadioButtonv7;
+	private JPanel panelRadio,panelv15,panelv7;
+	private ButtonGroup grupBDs;
+	private JButton botaoBanco,botaoFTP;
+	private String Loja,UF;
+	
+	
+	public JanelaCreatedBD(int x,int y){
+		super("Geração de Banco de Dados");
+		setContentPane(new JPanel());
+		setResizable(false);
+		setVisible(true);
+		setLayout(null);
+		OrganizarRadio();
+		OrganizarPainelv15();
+		OrganizarPainelv7();
+		setMinimumSize(new Dimension(x,y));
+		setLocationRelativeTo(null);
+		Desabilitar();
+			for(int i = 0;i<listButtonsv15.size();i++) {
+				listButtonsv15.get(i).addActionListener(this);
+				listButtonsv15.get(i).setFont(new Font("Times New Roman", Font.BOLD, 11));
+				listButtonsv15.get(i).setBackground(Color.WHITE);
+			}
+			
+			for(int i = 0;i<listButtonsv7.size();i++){
+				listButtonsv7.get(i).addActionListener(this);
+				listButtonsv7.get(i).setFont(new Font("Times New Roman", Font.BOLD, 11));
+				listButtonsv7.get(i).setBackground(Color.WHITE);
+			}
+	}	
+	
+	
+	public void OrganizarRadio() {
+		
+		RadioButtonv15 = new JRadioButton("Banco de dados v15");
+		RadioButtonv7 = new JRadioButton("Banco de dados v7");
+		grupBDs = new ButtonGroup();
+		botaoBanco = new JButton("BD Gerados");
+		botaoFTP = new JButton("FTP");
+		grupBDs.add(RadioButtonv15);
+		grupBDs.add(RadioButtonv7);
+		panelRadio = new JPanel(new MigLayout());
+		panelRadio.setBorder(BorderFactory.createTitledBorder("Escolha a opção"));
+		panelRadio.setBounds(0, 0, 500, 60);
+		panelRadio.add(RadioButtonv15);
+		panelRadio.add(RadioButtonv7);
+		panelRadio.add(botaoBanco);
+		panelRadio.add(botaoFTP);
+		RadioButtonv15.addItemListener(this);
+		RadioButtonv7.addItemListener(this);
+		botaoBanco.addActionListener(this);
+		botaoFTP.addActionListener(this);
+		botaoBanco.setFont(new Font("Times New Roman", Font.BOLD, 11));
+		botaoBanco.setBackground(Color.WHITE);
+		botaoFTP.setFont(new Font("Times New Roman", Font.BOLD, 11));
+		botaoFTP.setBackground(Color.WHITE);
+
+
+		this.add(panelRadio);
+	}
+	
+	public void OrganizarPainelv15() {
+		panelv15 = new JPanel(new MigLayout());
+		panelv15.setBorder(BorderFactory.createTitledBorder("Banco de dados v15"));
+		panelv15.setBounds(0, 77, 700, 80);
+		listButtonsv15.add(new JButton("Mover Carga"));
+		listButtonsv15.add(new JButton("Config Ini"));
+		listButtonsv15.add(new JButton("Manutenção"));
+		listButtonsv15.add(new JButton("SegurançaFL"));
+		listButtonsv15.add(new JButton("Repara"));
+		listButtonsv15.add(new JButton("Compactar Arquivo"));
+
+		panelv15.add(listButtonsv15.get(0),"gapleft 50");
+		panelv15.add(listButtonsv15.get(1),"grow");
+		panelv15.add(listButtonsv15.get(2),"wrap");
+		panelv15.add(listButtonsv15.get(3),"gapleft 50");
+		panelv15.add(listButtonsv15.get(4),"grow");
+		panelv15.add(listButtonsv15.get(5),"grow");
+		this.add(panelv15);
+	}
+	
+	public void OrganizarPainelv7() {
+		panelv7 = new JPanel(new MigLayout());
+		panelv7.setBorder(BorderFactory.createTitledBorder("Banco de Dados v7"));
+		panelv7.setBounds(0, 170, 700, 80);
+		listButtonsv7.add(new JButton("Mover Carga"));
+		listButtonsv7.add(new JButton("Mover MDB"));
+		listButtonsv7.add(new JButton("Config Ini"));
+		listButtonsv7.add(new JButton("Segurança FL"));
+		listButtonsv7.add(new JButton("Repara"));
+		listButtonsv7.add(new JButton("Compactar Arquivo"));
+		
+		panelv7.add(listButtonsv7.get(0),"gapleft 50");
+		panelv7.add(listButtonsv7.get(1),"grow");
+		panelv7.add(listButtonsv7.get(2),"wrap");
+		panelv7.add(listButtonsv7.get(3),"gapleft 50");
+		panelv7.add(listButtonsv7.get(4),"grow");
+		panelv7.add(listButtonsv7.get(5),"grow");
+		this.add(panelv7);
+	}
+	
+	public void Desabilitar(){
+			for(int i=0; i<listButtonsv15.size();i++) {
+				listButtonsv15.get(i).setEnabled(false);
+			}
+			for(int i=0; i<listButtonsv7.size();i++) {
+				listButtonsv7.get(i).setEnabled(false);
+			}	
+			
+			botaoFTP.setEnabled(false);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		if(e.getSource() == listButtonsv15.get(0)){
+			MovimentarCargav15();
+			listButtonsv15.get(0).setEnabled(false);
+		}	else if(e.getSource() == listButtonsv15.get(1)){
+			new Comando("cmd /c C:\\Cosmos\\Exe\\ConfigIni -"+ System.getProperty("user.name")+".exe");
+		}	else if(e.getSource() == listButtonsv15.get(2)){
+			new Comando("cmd /c C:\\Cosmos\\Util\\ManutencaoSistema.exe");
+		}	else if(e.getSource() == listButtonsv15.get(3)) {
+			new Comando("cmd /c C:\\Cosmos\\Exe\\SegFl.exe");
+		}	else if(e.getSource() == listButtonsv15.get(4)) {
+			new Comando("cmd /c C:\\Cosmos\\Exe\\Repara.exe");
+			// excluir os bak pra ficar apenas 1
+			new Comando("cmd /c del C:\\Cosmos\\Bck\\*.bak");
+		}	else if(e.getSource() == listButtonsv15.get(5))	{
+				CompactarArquivov15();
+				listButtonsv15.get(5).setEnabled(false);
+				botaoFTP.setEnabled(true);
+		}	
+		//v7
+		if(e.getSource() == listButtonsv7.get(0)){
+			MoverArquivov7();
+			listButtonsv7.get(0).setEnabled(false);
+		}	else if(e.getSource() == listButtonsv7.get(1)){
+			new Comando("cmd /c copy \"C:\\Cosmos\\Estrutura de Versoes\\Estrutura 7.0\\Csmloja.mdb C:\\Cosmos\\Dat\\ \"");
+		}	else if(e.getSource() == listButtonsv7.get(3)){
+			new Comando("cmd /c C:\\Cosmos\\Exe\\ConfigIni -"+ System.getProperty("user.name")+".exe");
+		}  	else if(e.getSource() == listButtonsv7.get(3)) {
+			new Comando("cmd /c C:\\Cosmos\\Exe\\SegFl.exe");
+		}	else if(e.getSource() == listButtonsv7.get(4)) {
+			new Comando("cmd /c C:\\Cosmos\\Exe\\Repara.exe");
+		}	else if(e.getSource() == listButtonsv7.get(5))	{
+			CompactarArquivov7();
+			listButtonsv7.get(5).setEnabled(false);
+			botaoFTP.setEnabled(true);
+		}
+		
+		
+			if(e.getSource() == botaoBanco){
+			new Comando("cmd /c explorer C:\\ToolsWork\\Backup_Bancos\\");
+		} 	
+			else if(e.getSource() == botaoFTP) {
+			this.UF = JOptionPane.showInputDialog("Digite o estado da loja: ");
+			while(UF.length() <= 2){
+				JOptionPane.showMessageDialog(null, "Estado invalido");
+				this.UF = JOptionPane.showInputDialog("Digite o estado da loja " +this.UF+ " novamente: ");
+				if (JOptionPane.CANCEL_OPTION == 1)
+					return;
+			}
+			if(RadioButtonv7.isSelected()) {
+			new Comando("cmd /c explorer ftp://"+System.getProperty("user.name")+"@ftpsuporte.pmenos.com.br//SuporteTecnico//"+this.UF);
+			}
+			else if(RadioButtonv15.isSelected())
+			new Comando("cmd /c explorer ftp://"+System.getProperty("user.name")+"@ftpsuporte.pmenos.com.br//SuporteTecnico//"+this.UF);
+			}
+			
+			
+		
+}
+	
+	public void MovimentarCargav15() {	
+		Date hoje = new Date();
+		SimpleDateFormat df;
+		df = new SimpleDateFormat("ddMM");
+		
+		String Arquivo_Carga = "RF"+Loja+df.format(hoje)+".zip";
+	
+		new Comando("cmd /c move F:\\Exporta\\"+Arquivo_Carga+" C:\\Cosmos\\Importa");
+		// recorta todos os bancos gerados para a pasta Backup - OK
+		new Comando("cmd /c move C:\\Cosmos\\Bck\\*.zip C:\\ToolsWork\\Backup_Bancos\\");
+		// exclui todas os bancos da pasta "Bck" - OK
+		new Comando("cmd /c del C:\\Cosmos\\Bck\\*.bak");
+		// copia as cargas do importa para uma pasta de armazenamento de backup das cargas - OK
+		new Comando("cmd /c copy C:\\Cosmos\\Importa\\"+Arquivo_Carga+" C:\\ToolsWork\\Cargas_Banco");
+	}
+	/*
+	public void CompactarArquivov15() {
+	
+		String arquivoCompactado = "Banco"+Loja+"_versao15.zip";
+		// criando zip - vai zipar todos os arquivos .bak - por esse motivo não posso deixar mais de
+		// 1 arquivo com essa mesma extensão
+		JProgressBar progresso = new JProgressBar();
+		setSize(300,100);
+		add(progresso);
+		setLocationRelativeTo(null);
+		setCursor(new Cursor(Cursor.WAIT_CURSOR));
+		setVisible(true);
+		progresso.setToolTipText("Aguarde...");
+		new Comando("cmd /c C:\\progra~1\\WinRAR\\Rar.exe a -ep C:\\Cosmos\\Bck\\"+arquivoCompactado+ " C:\\Cosmos\\Bck\\*.bak");
+		setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+		JOptionPane.showMessageDialog(this, "Carregamento concluído");
+		// movendo pro backup
+		new Comando("cmd /c move C:\\Cosmos\\Bck\\*.zip C:\\ToolsWork\\Backup_Bancos");
+		// abrindo pasta pro operador olhar
+		new Comando("cmd /c explorer C:\\Cosmos\\Bck\\");
+		
+		JOptionPane.showMessageDialog(null, "Você concluiu a geração do banco versão 15, cole o arquivo "+arquivoCompactado+" no FTP");
+	}
+	*/
+	
+	public void CompactarArquivov15() {
+		
+		String arquivoCompactado = "Banco"+Loja+"_versao15.zip";
+		
+		 try {
+	           Compactador.compactarParaZip("C:\\Cosmos\\Bck\\"+arquivoCompactado,"C:\\Cosmos\\Bck\\",".bak");
+	       } catch (IOException e) {
+	           // TODO Auto-generated catch block
+	           e.printStackTrace();
+	       }
+	       
+	   	new Comando("cmd /c copy C:\\Cosmos\\Bck\\*.zip C:\\ToolsWork\\Backup_Bancos");
+	   	new Comando("cmd /c explorer C:\\Cosmos\\Bck\\");
+	   	
+		JOptionPane.showMessageDialog(null, "Você concluiu a geração do banco versão 15, cole o arquivo "+arquivoCompactado+" no FTP");
+	}
+	
+	public void MoverArquivov7() {
+		Date hoje = new Date();
+		SimpleDateFormat df;
+		df = new SimpleDateFormat("ddMM");
+		
+		String Arquivo_Carga = "RF"+Loja+df.format(hoje)+".zip";
+		System.out.println(Arquivo_Carga);
+		
+		new Comando("cmd /c move F:\\Exporta\\"+Arquivo_Carga+".zip "+" C:\\Cosmos\\Importa");
+		new Comando("cmd /c copy C:\\Cosmos\\Importa\\"+Arquivo_Carga+" C:\\ToolsWork\\Cargas_Banco");
+		new Comando("cmd /c move C:\\Cosmos\\Dat\\*.zip C:\\ToolsWork\\Backup_Bancos\\");
+		new Comando("cmd /c del C:\\Cosmos\\Dat\\*.mdb");
+	}
+	
+	public void CompactarArquivov7() {
+		String arquivoCompactado = "Banco"+Loja+"_versao7.zip";
+		 try {
+	           Compactador.compactarParaZip("C:\\Cosmos\\Dat\\"+arquivoCompactado,"C:\\Cosmos\\Dat\\",".mdb");
+	       } catch (IOException e) {
+	           // TODO Auto-generated catch block
+	           e.printStackTrace();
+	       }
+	   	new Comando("cmd /c copy C:\\Cosmos\\Dat\\*.zip C:\\ToolsWork\\Backup_Bancos");
+	   	new Comando("cmd /c explorer C:\\Cosmos\\Dat\\");
+	   	
+		JOptionPane.showMessageDialog(null, "Você concluiu a geração do banco versão 7, cole "+arquivoCompactado+" no FTP");
+	}
+	
+		
+
+	@Override
+	public void itemStateChanged(ItemEvent arg0) {
+		
+		if(RadioButtonv7.isSelected()){
+			Loja = JOptionPane.showInputDialog("Digite a loja: ");
+	
+			while(Integer.parseInt(Loja) < 0 || Integer.parseInt(Loja) > 1000){
+				Loja = JOptionPane.showInputDialog("Digite a loja novamente: ");
+			}
+			
+			int confirm = JOptionPane.showConfirmDialog(null, "Certifique se você gerou a carga corretamente, antes de começar as etapas. Deseja mover o arquivo da loja: " + Loja + "?","Verifique a loja",JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+			
+			if(confirm == JOptionPane.NO_OPTION){
+				Loja = JOptionPane.showInputDialog("Digite a loja novamente: ");
+
+				while(Integer.parseInt(Loja) < 0 || Integer.parseInt(Loja) > 1000 || Loja.length() != 3){
+					JOptionPane.showMessageDialog(null, "Inserção inválida");
+					Loja = JOptionPane.showInputDialog("Digite a loja novamente: ");
+				}
+			}			
+			
+			for(int i = 0;i<listButtonsv7.size();i++)
+				listButtonsv7.get(i).setEnabled(true);
+			
+		} else if(RadioButtonv7.isSelected() == false){
+			Desabilitar();
+		}
+		
+		if(RadioButtonv15.isSelected()){
+			Loja = JOptionPane.showInputDialog("Digite a loja: ");
+			
+			while(Integer.parseInt(Loja) < 0 || Integer.parseInt(Loja) > 1000 || Loja.length() != 3){
+				JOptionPane.showMessageDialog(null, "Inserção inválida");
+				Loja = JOptionPane.showInputDialog("Digite a loja novamente: ");
+			}
+			
+			int confirm = JOptionPane.showConfirmDialog(null, "Certifique se você gerou a carga corretamente, antes de começar as etapas. Deseja mover o arquivo da loja: " + Loja + "?","Verifique a loja",JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+
+			if(confirm == JOptionPane.NO_OPTION){
+				Loja = JOptionPane.showInputDialog("Digite a loja novamente: ");
+
+				while(Integer.parseInt(Loja) < 0 || Integer.parseInt(Loja) > 1000 || Loja.length() != 3){
+					JOptionPane.showMessageDialog(null, "Inserção inválida");
+					Loja = JOptionPane.showInputDialog("Digite a loja novamente: ");
+				}
+			}
+			
+	
+			for(int i = 0;i<listButtonsv15.size();i++)
+				listButtonsv15.get(i).setEnabled(true);
+
+			
+		} 
+
+	
+}
+	}
+	
